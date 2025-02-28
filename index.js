b4b9b4fe865b92a69d1f121a7e22a829a3f1a41e
ndevu3399 <wachiramwas122@gmail.com>
2025-02-28 21:35:50 +0300

    modify index.js index.html

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+addingEventListener(); // Ensure the function is called to attach the event listener
